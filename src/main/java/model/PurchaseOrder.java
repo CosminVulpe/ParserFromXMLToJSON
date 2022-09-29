0ac323068665354e10e564456a085956b0922c0c
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-09-29 15:13:15 +0300

    Add PurchaseOrder Class

---
+++ src/main/java/model/PurchaseOrder.java
@@ -0,0 +1,74 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import sun.jvm.hotspot.debugger.Address;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.List;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PurchaseOrder {
+
+    @XmlAttribute(name = "PurchaseOrderNumber")
+    private String purchaseOrderNumber;
+    @XmlAttribute(name = "OrderDate")
+    @JsonProperty(value = "OrderDate")
+    private String orderDate;
+    @XmlElement(name = "Address")
+    @JsonProperty(value = "Address")
+    private List<Address> addressList;
+    @XmlElement(name = "DeliveryNotes")
+    @JsonProperty(value = "DeliveryNotes")
+    private String deliveryNote;
+    @XmlElement(name = "Items")
+    @JsonProperty(value = "Items")
+    private PurchaseItem purchaseItem;
+
+
+    public PurchaseOrder() {
+    }
+
+    public String getPurchaseOrderNumber() {
+        return purchaseOrderNumber;
+    }
+
+    public void setPurchaseOrderNumber(String purchaseOrderNumber) {
+        this.purchaseOrderNumber = purchaseOrderNumber;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getDeliveryNote() {
+        return deliveryNote;
+    }
+
+    public void setDeliveryNote(String deliveryNote) {
+        this.deliveryNote = deliveryNote;
+    }
+
+    public List<Address> getAddressList() {
+        return addressList;
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList = addressList;
+    }
+
+
+    public List<Item> getPurchaseItem() {
+        return purchaseItem.getItemList();
+    }
+
+    public void setPurchaseItem(PurchaseItem purchaseItem) {
+        this.purchaseItem = purchaseItem;
+    }
+}
